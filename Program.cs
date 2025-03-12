c205810a9cc8837160376c47106525d66e040eeb
bror <reachingbror@gmail.com>
2025-03-12 17:39:23 +0100

    Now did the part 4 from the course, Strings.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
-﻿string name = "Bror"; 
+﻿//Create Read Update Delete
+//CRUD
 
-long richDude = 19600000000;
+//Create
+using System.Text;
 
-var charchar = "c";
+string petFish = "puffer fish";
+
+//Concatenation
+petFish = "porcupine " + petFish;
+
+//Template Literal
+Console.WriteLine($"I AM BUYING {petFish}. YOU CANNOT STOP ME DUDE!!!");
+
+
+Console.WriteLine(petFish);
+
+//READ
+//Console.Writeline()
+
+
+//UPDATE
+//petFish[0] = "P";
+string newPetFish = petFish.Replace("porcupine", "blue dot");
+Console.WriteLine(newPetFish);
+
+
+//DELETE
+//String Builder
+StringBuilder newCrustacean = new StringBuilder();
+newCrustacean.Append("fiddler crab");
+Console.WriteLine(newCrustacean);
+newCrustacean.Remove(0,8);
+Console.WriteLine(newCrustacean);
